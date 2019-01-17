8a90929568f6904eec3e46ba660e3b6f0bffef48
ChenmingLi <17858975663@163.com>
2019-01-17 16:02:06 +0800

    全局请求提示的增加
    全局请求提示的增加

--- src/service/service.js
+++ src/service/service.js
@@ -1,14 +1,19 @@
 // import Vue from 'vue'
 import axios from 'axios'
 import router from '../router';
+import { Toast } from 'vant';
 //  设置默认请求头
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-//  设置请求超时之前的毫秒数
-axios.defaults.timeout = 60000
 
-//  请求拦截器
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+    //  设置请求超时之前的毫秒数
+axios.defaults.timeout = 60000
+    //  请求拦截器
 axios.interceptors.request.use(
     config => {
+        Toast.loading({
+            mask: true,
+            message: '加载中...'
+        });
         config.headers = {
             'token': localStorage.getItem('token') || '' // 请求带上token
         }
@@ -20,10 +25,11 @@ axios.interceptors.request.use(
     }
 )
 axios.interceptors.response.use((data) => {
-
+    Toast.clear();
     if (data.headers['customize-token']) {
         localStorage.setItem('customize-token', data.headers['customize-token'])
     }
+    return data
 }, error => {
     return Promise.reject(error)
 })
--- src/service/serviceLcm.js
+++ src/service/serviceLcm.js
@@ -6,3 +6,27 @@ export function qryProduct(promise) {
         return res.data;
     })
 }
+// 点击添加分类
+export function addClassification(promise) {
+    return axios.post(api + '/app/eat/addClassification', promise).then(res => {
+        return res.data;
+    })
+}
+// 添加商品
+export function addProduct(promise) {
+    return axios.post(api + '/app/eat/addProduct', promise).then(res => {
+        return res.data;
+    })
+}
+// 删除分类
+export function delClassification(promise) {
+    return axios.post(api + '/app/eat/delClassification', promise).then(res => {
+        return res.data;
+    })
+}
+// 删除商品
+export function delProduct(promise) {
+    return axios.post(api + '/app/eat/delProduct', promise).then(res => {
+        return res.data;
+    })
+}
